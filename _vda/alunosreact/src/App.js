e704e001c39e5de7eaa3aa933ba2de8153bfd773
EvandroBastos <evandrobastosviana@gmail.com>
2021-09-10 01:38:47 -0300

    react incluso commit

---
+++ _vda/alunosreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <h1>EvandroBastos.Net</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
